7c76dd13d318be42dae8cb42f9e7851556e55043
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-19 21:13:29 +0300

    v 0.18

--- Assets/Scripts/TemplateScripts/ObjectPool.cs
+++ Assets/Scripts/TemplateScripts/ObjectPool.cs
@@ -10,6 +10,7 @@ public class ObjectPool : MonoSingleton<ObjectPool>
     {
         public Queue<GameObject> pooledObjects;
         public GameObject objectPrefab;
+        [HideInInspector] public GameObject objectParent;
         public int poolSize;
     }
 
@@ -17,15 +18,22 @@ public class ObjectPool : MonoSingleton<ObjectPool>
 
     private void Awake()
     {
+        GameObject parentObject = new GameObject("ObjectPool");
+
         for (int i1 = 0; i1 < pools.Length; i1++)
         {
             pools[i1].pooledObjects = new Queue<GameObject>();
+
+            GameObject queueParent = new GameObject(pools[i1].objectPrefab.name);
+            queueParent.transform.SetParent(parentObject.transform);
+            pools[i1].objectParent = queueParent;
 
             for (int i2 = 0; i2 < pools[i1].poolSize; i2++)
             {
                 GameObject obj = Instantiate(pools[i1].objectPrefab);
                 obj.SetActive(false);
                 pools[i1].pooledObjects.Enqueue(obj);
+                obj.transform.SetParent(queueParent.transform);
             }
         }
     }
@@ -37,9 +45,41 @@ public class ObjectPool : MonoSingleton<ObjectPool>
         pools[objectType].pooledObjects.Enqueue(obj);
         return obj;
     }
+    public GameObject GetPooledObjectAdd(int objectType, Vector3 objectPos)
+    {
+        GameObject obj = pools[objectType].pooledObjects.Dequeue();
+        obj.transform.position = objectPos;
+        obj.SetActive(true);
+        pools[objectType].pooledObjects.Enqueue(obj);
+        return obj;
+    }
+    public GameObject GetPooledObjectAdd(int objectType, Vector3 objectPos, Vector3 objectRotation)
+    {
+        GameObject obj = pools[objectType].pooledObjects.Dequeue();
+        obj.transform.position = objectPos;
+        obj.transform.rotation = Quaternion.Euler(objectRotation);
+        obj.SetActive(true);
+        pools[objectType].pooledObjects.Enqueue(obj);
+        return obj;
+    }
     public GameObject GetPooledObject(int objectType)
     {
         GameObject obj = pools[objectType].pooledObjects.Dequeue();
+        obj.SetActive(true);
+        return obj;
+    }
+    public GameObject GetPooledObject(int objectType, Vector3 objectPos)
+    {
+        GameObject obj = pools[objectType].pooledObjects.Dequeue();
+        obj.transform.position = objectPos;
+        obj.SetActive(true);
+        return obj;
+    }
+    public GameObject GetPooledObject(int objectType, Vector3 objectPos, Vector3 objectRotation)
+    {
+        GameObject obj = pools[objectType].pooledObjects.Dequeue();
+        obj.transform.position = objectPos;
+        obj.transform.rotation = Quaternion.Euler(objectRotation);
         obj.SetActive(true);
         return obj;
     }
